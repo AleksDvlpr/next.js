99157cc5244bee2e9f0f8988633660e7caecb0bb
Aleksandr Skokov <skokov87@gmail.com>
2024-09-08 12:55:33 +0300

    integrated styled-components

--- src/app/client/BackButton.js
+++ src/app/client/BackButton.js
@@ -1,8 +1,9 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { StyledButton } from '@/app/client/styles';
 
 export default function BackButton() {
   const router = useRouter();
-  return <button onClick={() => router.back()}>Back</button>;
+  return <StyledButton onClick={() => router.back()}>Back</StyledButton>;
 }
---
+++ src/app/client/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  font-weight: bold;
+`;
+
+export { StyledButton };
